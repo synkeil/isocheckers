cdc55d18966dd764abbcf516842d0e8ef1ab2a70
nicolas "sinkeil" b <NBOURGAREL@GMAIL.COM>
2016-06-27 19:01:37 +0200

    adding characters, going the fftactics route

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
-$(".pop_main").click(function () {
-    if ($(this).hasClass('selected')) {
-        $(this).removeClass('selected');
-    } else {
-        $(this).addClass('selected');
-    }
-});
+$(function () {
+            $(".pop_main").click(function () {
+                if ($(this).hasClass('selected')) {
+                    $(this).removeClass('selected');
+                } else {
+                    $(this).addClass('selected');
+                }
+            });
+
+            var bird = false;
+            var sinkeil = false;
+
+            $('#birdskull').click(function () {
+                bird = true;
+                console.log(bird);
+            });
+            console.log(bird);
+
+            $('#sinkeil').click(function () {
+                sinkeil = true;
+                console.log(sinkeil);
+            });
+            console.log(sinkeil);
+
+
+            //
+            //$('.popper_prime').hover(function(){
+            //    $(this '> .pop_main').css('content','url(svg/birdskull.svg)');
+          });
