29cebe08131780853cbb26ed5dba3c85043fe878
nicolas "sinkeil" b <NBOURGAREL@GMAIL.COM>
2016-07-01 18:02:29 +0200

    keeping up with master

--- js/script.js
+++ js/script.js
@@ -1,73 +1,167 @@
 $(function () {
-    //adding selected class on click
-    $(".popper_prime").click(function () {
-        if ($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
-        } else {
-            $(this).addClass('selected');
-        }
-    });
+
 
     //initiating the chars
     var bird = false;
     var sinkeil = false;
-    var Slct = "";
+    var Switched = 0;
+    var charClass = ["pointer", "bird", "sinkeil"];
+    var Url = ["url(svg/pointer_hover.svg)", "url(svg/birdskull.svg)", "url(svg/sinkeil.svg)"];
+    var Slct = charClass[Switched];
 
-    //activating the bird
-    $('#birdskull').click(function () {
-        sinkeil = false;
-        bird = true;
-        console.log(bird);
-    });
+    var Sync = true;
 
-    //activating the keil
-    $('#sinkeil').click(function () {
-        bird = false;
-        sinkeil = true;
-        console.log(sinkeil);
-    });
-
-//  defining wich character is selected
-    function charslct() {
+    //setting the switch depending on the selected char
+    function Switch() {
         if (bird == true) {
-            Slct = "url(svg/birdskull.svg)";
+            Switched = 1;
         } else {
             if (sinkeil == true) {
-                Slct = "url(svg/sinkeil.svg)";
+                Switched = 2;
             } else {
-                Slct = "url(svg/pointer_hover.svg)";
+                Switched = 0;
             }
         }
     }
 
-    //show the selected char on hover
-    $('.popper_prime').mouseenter(function () {
-        charslct();
-        $('.support', this).attr({
-            style: "content:" + Slct + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(2%) translateZ(0px);"
-        });
+    //checking if the selected element is the same as the hovered tile's content
+
+    function synCheck(elem) {
+        Switch();
+        console.log("class is "+charClass[Switched])
+        console.log("sinkeil " + sinkeil);
+        console.log("bird " + bird);
+        if ($(elem).hasClass('selected')) {
+            if (($(elem).hasClass('bird') && bird == true) || ($(elem).hasClass('sinkeil') && sinkeil == true) || (Switched == 0)) {
+                Sync = true;
+            } else {
+                Sync = false;
+            }
+        } else {
+            Sync = true;
+        }
+    }
+
+    //adding class representing selected char on click
+
+    function CharOnClick(elem) {
+        if ($(elem).hasClass('selected') && (Sync == true)) {
+            $(elem).addClass(charClass[Switched]);
+        } else {
+            if ($(elem).hasClass('selected')) {
+                return;
+            } else {
+                $(elem).removeClass(charClass[Switched]);
+            }
+        }
+
+    }
+
+
+
+    //activating the bird
+
+    $('#birdskull').click(function () {
+        sinkeil = false;
+        bird = true;
+        Switch();
     });
-    //manage the tile's state on mouse out
-    $('.popper_prime').mouseleave(function () {
-        
-        if ($(this).hasClass('selected')) {
-            $('.support', this).attr({
-                style: "content:" + Slct + ";"
-            });
+
+    //activating the keil
+
+    $('#sinkeil').click(function () {
+        bird = false;
+        sinkeil = true;
+        Switch();
+    });
+
+
+    //adding selected class on click
+
+    $(".popper_prime").click(function () {
+        synCheck(this);
+        CharOnClick(this);
+        if ($(this).hasClass('selected') && Sync == true) {
+            $(this).removeClass(charClass[Switched]);
+            $(this).removeClass('selected');
+        } else {
+            if ($(this).hasClass('selected')) {
+                return;
+            } else {
+                $(this).addClass(charClass[Switched]);
+                $(this).addClass('selected');
+            }
+        }
+    });
+
+
+    //Set the character selected on the clicked tile
+
+    $('.popper_prime').click(function () {
+        synCheck(this);
+        CharOnClick(this);
+        if (Sync == false) {
+            return;
         } else {
             $('.support', this).attr({
-                style: "content:'';"
+                style: "content:" +Url[Switched]+ ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(0px);"
+            })
+        }
+    });
+
+    //show the selected char on hover
+
+    $('.popper_prime').mouseenter(function () {
+        synCheck(this);
+        if ($(this).hasClass('selected')) {
+            if ($(this).hasClass('bird')) {
+                $('.support', this).attr({
+                    style: "content:" + Url[1] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(45px);"
+                });
+            } else {
+                if ($(this).hasClass('sinkeil')) {
+                    $('.support', this).attr({
+                        style: "content:" + Url[2] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(45px);"
+                    });
+                } else {
+                    $('.support', this).attr({
+                        style: "content:" + Url[0] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(45px);"
+                    });
+                }
+            }
+        } else {
+            $('.support', this).attr({
+                style: "content:" +Url[Switched]+ ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(45px);"
+            });
+        }
+        console.log("Sync " + Sync);
+
+    });
+    //manage the tile's state on mouse out
+
+    $('.popper_prime').mouseleave(function () {
+        if ($(this).hasClass('selected')) {
+            if ($(this).hasClass('bird')) {
+                $('.support', this).attr({
+                    style: "content:" + Url[1] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-30%) translateX(0%) translateZ(0px);"
+                });
+            } else {
+                if ($(this).hasClass('sinkeil')) {
+                    $('.support', this).attr({
+                        style: "content:" + Url[2] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-30%) translateX(0%) translateZ(0px);"
+                    });
+                } else {
+                    $('.support', this).attr({
+                        style: "content:" + Url[0] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-30%) translateX(0%) translateZ(0px);"
+                    });
+                }
+            }
+
+        } else {
+            $('.support', this).attr({
+                style: "content:'';transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-30%) translateX(0%) translateZ(0px);"
             });
         }
 
     });
-
-    //Set the character selected on the clicked tile
-    $('.popper_prime').click(function () {
-        charslct();
-        $('.support', this).attr({
-            style: "content:" + Slct + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(2%) translateZ(0px);"
-        });
-    });
-    charslct();
 });
