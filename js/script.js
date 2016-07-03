33e465eb175e53c9ef91c0c47e2e4f8d6a7cea10
nicolas "sinkeil" b <NBOURGAREL@GMAIL.COM>
2016-07-03 23:53:18 +0200

    cleaning code and upgrading the bird

--- js/script.js
+++ js/script.js
@@ -1,51 +1,45 @@
 $(function () {
     //initiating the chars
-    var bird = false;
-    var sinkeil = false;
-    var Switched = 0;
-    var charClass = ["pointer", "bird", "sinkeil"];
-    var Url = ["url(svg/pointer_hover.svg)", "url(svg/birdskull.svg)", "url(svg/sinkeil.svg)"];
-    var Slct = charClass[Switched];
-    var Sync = true;
+    var bird,
+        sinkeil = false,
+        Switched = 0,
+        charClass = ["pointer", "bird", "sinkeil"],
+        Url = ["url(svg/pointer_hover.svg)", "url(svg/birdskull.svg)", "url(svg/sinkeil.svg)"],
+        Slct = charClass[Switched],
+        Sync = true,
+        count = 0,
+        Panel = ["#panel1", "#panel2", "#panel3"];
     //setting the switch depending on the selected char
     function Switch() {
-        if (bird == true) {
+        if (bird === true) {
             Switched = 1;
-        }
-        else {
-            if (sinkeil == true) {
-                Switched = 2;
-            }
-            else {
-                Switched = 0;
-            }
+        } else if (sinkeil === true) {
+            Switched = 2;
+        } else {
+            Switched = 0;
         }
     }
     //checking if the selected element is the same as the hovered tile's content
     function synCheck(elem) {
         Switch();
         if ($(elem).hasClass('selected')) {
-            if (($(elem).hasClass('bird') && bird == true) || ($(elem).hasClass('sinkeil') && sinkeil == true) || (Switched == 0)) {
+            if (($(elem).hasClass('bird') && bird === true) || ($(elem).hasClass('sinkeil') && sinkeil === true) || (Switched === 0)) {
                 Sync = true;
-            }
-            else {
+            } else {
                 Sync = false;
             }
-        }
-        else {
+        } else {
             Sync = true;
         }
     }
     //adding class representing selected char on click
     function CharOnClick(elem) {
-        if ($(elem).hasClass('selected') && (Sync == true)) {
+        if ($(elem).hasClass('selected') && (Sync === true)) {
             $(elem).addClass(charClass[Switched]);
-        }
-        else {
+        } else {
             if ($(elem).hasClass('selected')) {
                 return;
-            }
-            else {
+            } else {
                 $(elem).removeClass(charClass[Switched]);
             }
         }
@@ -66,15 +60,13 @@ $(function () {
     $(".popper_prime").click(function () {
         synCheck(this);
         CharOnClick(this);
-        if ($(this).hasClass('selected') && Sync == true) {
+        if ($(this).hasClass('selected') && Sync === true) {
             $(this).removeClass(charClass[Switched]);
             $(this).removeClass('selected');
-        }
-        else {
+        } else {
             if ($(this).hasClass('selected')) {
                 return;
-            }
-            else {
+            } else {
                 $(this).addClass(charClass[Switched]);
                 $(this).addClass('selected');
             }
@@ -84,13 +76,12 @@ $(function () {
     $('.popper_prime').click(function () {
         synCheck(this);
         CharOnClick(this);
-        if (Sync == false) {
+        if (Sync === false) {
             return;
-        }
-        else {
+        } else {
             $('.support', this).attr({
                 style: "content:" + Url[Switched] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(0px);"
-            })
+            });
         }
     });
     //show the selected char on hover
@@ -101,21 +92,18 @@ $(function () {
                 $('.support', this).attr({
                     style: "content:" + Url[1] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(45px);"
                 });
-            }
-            else {
+            } else {
                 if ($(this).hasClass('sinkeil')) {
                     $('.support', this).attr({
                         style: "content:" + Url[2] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(45px);"
                     });
-                }
-                else {
+                } else {
                     $('.support', this).attr({
                         style: "content:" + Url[0] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(45px);"
                     });
                 }
             }
-        }
-        else {
+        } else {
             $('.support', this).attr({
                 style: "content:" + Url[Switched] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-60%) translateX(0%) translateZ(45px);"
             });
@@ -128,67 +116,50 @@ $(function () {
                 $('.support', this).attr({
                     style: "content:" + Url[1] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-30%) translateX(0%) translateZ(0px);"
                 });
-            }
-            else {
+            } else {
                 if ($(this).hasClass('sinkeil')) {
                     $('.support', this).attr({
                         style: "content:" + Url[2] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-30%) translateX(0%) translateZ(0px);"
                     });
-                }
-                else {
+                } else {
                     $('.support', this).attr({
                         style: "content:" + Url[0] + ";transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-30%) translateX(0%) translateZ(0px);"
                     });
                 }
             }
-        }
-        else {
+        } else {
             $('.support', this).attr({
                 style: "content:'';transform:rotateX(-50deg) rotateY(40deg) rotateZ(-30deg) translateY(-30%) translateX(0%) translateZ(0px);"
             });
         }
     });
     //start the count
-    var count = 0;
-    var Panel = ["#panel1", "#panel2", "#panel3"];
     
     $('#panelButton1').click(function () {
         count = 0;
-        console.log("current count is " + count);
     });
-    
     $('#panelButton2').click(function () {
         count = 1;
-        console.log("current count is " + count);
     });
-    
     $('#panelButton3').click(function () {
         count = 2;
-        console.log("current count is " + count);
     });
-    
     //cicle the counter to get the previous pannel
     $('#prev').click(function () {
         if (count > 0) {
             count--;
-        }
-        else {
+        } else {
             count = (Panel.length - 1);
         }
         $(this).attr("href", Panel[count]);
-        console.log("there is "+ Panel.length +"panels");
     });
-    
     //cicle the counter to get the next pannel
     $('#next').click(function () {
         if (count < (Panel.length - 1)) {
             count++;
-        }
-        else {
+        } else {
             count = 0;
         }
         $(this).attr("href", Panel[count]);
-        console.log("there is "+ Panel.length +"panels");
     });
-    
 });
